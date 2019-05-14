2b1e2f5da61a35cf78d4cc9680e09858c2dbd274
jungjai <950222@naver.com>
2019-05-14 19:13:59 +0900

    Add error handler & body-paser module

--- app.js
+++ app.js
@@ -1,8 +1,28 @@
-var http = require('http');
+var express = require('express')
+var bodyParser = require('body-parser')
+var router = require('./router/index')
 
-http.createServer(function(request, response) {
-  response.writeHead(200);
-  response.write("Hello, this is dog.");
-  response.end();
-}).listen(3000);
-console.log('Listening on port 3000...');
+var app = express()
+app.listen(3000, function(){
+  console.log("start! sogong server on 3000")
+})
+app.use(express.static('public'));
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended:true})) //ps모듈 사용
+app.use(router)
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
